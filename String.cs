241747db3f6a622696f9889d4b717b2169ceaa3b
doluig <53130663+doluig@users.noreply.github.com>
2019-09-04 20:51:09 -0500

    Add files via upload

---
+++ String.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "The quick brown fox jumps over the lazy dog".ToUpper();
+            Console.Write("Ingrese la palabra: ");
+           
+            string palabra = Console.ReadLine().ToUpper();
+           
+            for (int i = 0; i < frase.Length - palabra.Length+1; i++)
+            {
+                string x = "";
+
+                for (int j = 0; j < palabra.Length; j++)
+                {
+
+                    x += frase[i + j];
+
+                    if (x == palabra)
+                    {
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine(palabra + "vs" + x);
+                }
+
+            }
+        }
+    }
+    
+}
