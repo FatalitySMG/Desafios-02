2bf13b3917471acf824e4e2f20a0117c572d7879
doluig <53130663+doluig@users.noreply.github.com>
2019-10-02 23:49:51 -0500

    Add files via upload

---
+++ TurretMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMovement : MonoBehaviour
+{
+    [SerializeField] float magAng = 180;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direccionAng = new Vector3(0, 1, 0);
+        float sentidoAng = Input.GetAxis("Turret");
+        Vector3 velocidadAng = magAng * direccionAng * sentidoAng;
+        Vector3 distanciaAng = velocidadAng * Time.deltaTime;
+        transform.eulerAngles += distanciaAng;
+    }
+}
